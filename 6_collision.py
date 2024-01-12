99a2e52c5c467e216a274ef26885d1c633577c5c
SH082 <sshkimssh@naver.com>
2024-01-12 11:28:51 +0900

    collision set

--- 6_collision.py
+++ 6_collision.py
@@ -31,6 +31,14 @@ to_y = 0
 #이동 속도
 character_speed = 0.6
 
+# 적 enemy 캐릭터
+enemy = pygame.image.load("/Users/ksh/Desktop/python/pygame/enemy.png")
+enemy_size = enemy.get_rect().size #이미지 크기 구해옴
+enemy_width = enemy_size[0] #캐릭터 가로 크기
+enemy_height = enemy_size[1] #캐릭터 세로 크기
+enemy_x_pos = screen_width/2- enemy_width/2 #화면 가로 절반 크기 아래
+enemy_y_pos = screen_height/2 - enemy_height/2 #화면 세로 절반 크기 아래
+
 # 이벤트 루프
 running = True #게임이 진행중인가?
 
@@ -71,10 +79,28 @@ while running:
     elif character_y_pos > screen_height - character_height:
         character_y_pos = screen_height - character_height
 
+    #충돌 처리를 위한 rect 정보 update
+    character_rect = character.get_rect()
+    character_rect.left = character_x_pos
+    character_rect.top = character_y_pos
+
+    enemy_rect = enemy.get_rect()
+    enemy_rect.left = enemy_x_pos
+    enemy_rect.top = enemy_y_pos
+
+    # 충돌 체크
+    if character_rect.colliderect(enemy_rect):
+        print("충돌했어요")
+        running = False
+
+    #요소 그리기
     screen.blit(background,(0,0)) #배경 그리기
     
     screen.blit(character,(character_x_pos,character_y_pos)) #캐릭터 그리기
     
+    screen.blit(enemy,(enemy_x_pos,enemy_y_pos)) #적 그리기
+
+
     pygame.display.update() #게임 화면을 다시 그리기 계속 반복
 #pygame 종료
 pygame.quit()
